e134cff60a9c2024e872ec13c3fe19480439bb8e
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2024-11-10 17:13:26 -0600

    Create FavoriteCard with model example

--- app/src/main/java/com/example/papalote_app/components/FavoriteCard.kt
+++ app/src/main/java/com/example/papalote_app/components/FavoriteCard.kt
@@ -1,6 +1,96 @@
 package com.example.papalote_app.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.papalote_app.model.Activity
+import com.example.papalote_app.model.getActivities
 
 @Composable
-fun FavoriteCard
+@Preview
+fun FavoriteCard(activity: Activity = getActivities()[1]) {
+    Card(
+        shape = RoundedCornerShape(corner = CornerSize(12.dp)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .padding(16.dp, 8.dp, 16.dp, 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                shape = RoundedCornerShape(12.dp, 0.dp, 0.dp, 12.dp),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(75.dp)
+            ) {
+                AsyncImage(
+                    model = activity.image,
+                    contentDescription = "Activity Image",
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp, 10.dp, 16.dp, 10.dp)
+            ) {
+                Text(
+                    text = activity.name,
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        color = Color(0xFF1D1B20),
+                        fontWeight = FontWeight(500)
+                    )
+                )
+                Text(
+                    text = activity.zone,
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        color = Color(0xFF615D67),
+                        fontWeight = FontWeight(400)
+                    )
+                )
+            }
+
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Notification",
+                tint = Color.Black,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(end = 20.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/papalote_app/screens/Favorites.kt
+++ app/src/main/java/com/example/papalote_app/screens/Favorites.kt
@@ -2,12 +2,20 @@ package com.example.papalote_app.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.papalote_app.components.EventCard
+import com.example.papalote_app.components.FavoriteCard
+import com.example.papalote_app.model.getActivities
+import com.example.papalote_app.model.getEvents
 
 @Composable
 fun Favorites(navController: NavController) {
@@ -17,6 +25,14 @@ fun Favorites(navController: NavController) {
             .background(Color.White)
     )
     {
-        Text("Favoritos")
+        Column() {
+            Text("Favoritos")
+        }
+        LazyColumn {
+            items(items = getActivities()) { activity ->
+                FavoriteCard(activity)
+            }
+
+        }
     }
 }
