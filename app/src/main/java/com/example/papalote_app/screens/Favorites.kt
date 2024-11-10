ccbeadc0b52fdc2145803f5626ce2719f9150ff3
JimenaCarmona2 <jimenagarcia0284@gmail.com>
2024-11-10 17:46:28 -0600

    Favorites files with each example activity in it

--- app/src/main/java/com/example/papalote_app/screens/Favorites.kt
+++ app/src/main/java/com/example/papalote_app/screens/Favorites.kt
@@ -4,18 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.papalote_app.components.EventCard
 import com.example.papalote_app.components.FavoriteCard
 import com.example.papalote_app.model.getActivities
-import com.example.papalote_app.model.getEvents
 
 @Composable
 fun Favorites(navController: NavController) {
@@ -26,7 +27,14 @@ fun Favorites(navController: NavController) {
     )
     {
         Column() {
-            Text("Favoritos")
+            Text(
+                text = "Favoritos",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    color = Color(0xFF1D1B20),
+                    fontWeight = FontWeight(600)
+                ),
+                modifier = Modifier.padding(32.dp, 16.dp, 32.dp, 16.dp)
+            )
         }
         LazyColumn {
             items(items = getActivities()) { activity ->
