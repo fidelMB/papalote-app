968dcceb28d7caf68f91aad26c04483b6cfa40ad
Fidel Morales <fidel.morales.briones@gmail.com>
2024-11-09 19:53:33 -0600

    Events screen ui ready

--- app/src/main/java/com/example/papalote_app/model/Event.kt
+++ app/src/main/java/com/example/papalote_app/model/Event.kt
@@ -24,31 +24,66 @@ fun getEvents(): List<Event> {
             zone = "Zona de Ciencias"
         ),
         Event(
-            name = "Taller de Manualidades Ecológicas",
+            name = "Taller de Manualidades",
             description = "Aprende a hacer manualidades usando materiales reciclados y participa en una charla sobre sostenibilidad.",
             date = "martes 22 nov 2024",
-            image = "https://example.com/images/eco_crafts.jpg",
+            image = "https://offloadmedia.feverup.com/monterreysecreto.com/wp-content/uploads/2022/08/08093043/papalote-museo-nin%CC%83o.jpg",
             zone = "Zona de Arte y Reciclaje"
         ),
         Event(
             name = "Aventura en el Planetario",
             description = "Un recorrido guiado por nuestro mini planetario, donde los niños pueden aprender sobre el sistema solar y las estrellas.",
             date = "martes 2 dic 2024",
-            image = "https://example.com/images/planetarium_adventure.jpg",
+            image = "https://sietecolores.mx/wp-content/uploads/2015/04/papalote-monterrey-exhibiciones-2-min.jpg",
             zone = "Planetario"
         ),
         Event(
             name = "Paseo de Exploración Natural",
             description = "Únete a nosotros en una caminata por la naturaleza para aprender sobre las plantas, animales y cómo los ecosistemas trabajan juntos.",
             date = "martes 9 dic 2024",
-            image = "https://example.com/images/nature_walk.jpg",
+            image = "https://sic.cultura.gob.mx/images/59144",
             zone = "Jardín de la Naturaleza"
         ),
         Event(
             name = "Taller de Alimentación Sostenible",
             description = "Una divertida sesión donde los niños aprenden sobre fuentes de alimentos sostenibles y prueban recetas simples y saludables.",
             date = "martes 16 dic 2024",
-            image = "https://example.com/images/sustainable_food.jpg",
+            image = "https://i.blogs.es/60546a/papalote/1366_2000.jpg",
+            zone = "Cocina Verde"
+        ),
+        Event(
+            name = "Día de Descubrimiento Científico",
+            description = "¡Explora las maravillas de la física, química y biología con divertidos experimentos prácticos!",
+            date = "martes 15 nov 2024",
+            image = "https://images.csmonitor.com/csm/2019/01/0121-L2MIX-touch-museum.jpg?alias=standard_900x600",
+            zone = "Zona de Ciencias"
+        ),
+        Event(
+            name = "Taller de Manualidades",
+            description = "Aprende a hacer manualidades usando materiales reciclados y participa en una charla sobre sostenibilidad.",
+            date = "martes 22 nov 2024",
+            image = "https://offloadmedia.feverup.com/monterreysecreto.com/wp-content/uploads/2022/08/08093043/papalote-museo-nin%CC%83o.jpg",
+            zone = "Zona de Arte y Reciclaje"
+        ),
+        Event(
+            name = "Aventura en el Planetario",
+            description = "Un recorrido guiado por nuestro mini planetario, donde los niños pueden aprender sobre el sistema solar y las estrellas.",
+            date = "martes 2 dic 2024",
+            image = "https://sietecolores.mx/wp-content/uploads/2015/04/papalote-monterrey-exhibiciones-2-min.jpg",
+            zone = "Planetario"
+        ),
+        Event(
+            name = "Paseo de Exploración Natural",
+            description = "Únete a nosotros en una caminata por la naturaleza para aprender sobre las plantas, animales y cómo los ecosistemas trabajan juntos.",
+            date = "martes 9 dic 2024",
+            image = "https://sic.cultura.gob.mx/images/59144",
+            zone = "Jardín de la Naturaleza"
+        ),
+        Event(
+            name = "Taller de Alimentación Sostenible",
+            description = "Una divertida sesión donde los niños aprenden sobre fuentes de alimentos sostenibles y prueban recetas simples y saludables.",
+            date = "martes 16 dic 2024",
+            image = "https://i.blogs.es/60546a/papalote/1366_2000.jpg",
             zone = "Cocina Verde"
         )
     )
--- app/src/main/java/com/example/papalote_app/screens/Events.kt
+++ app/src/main/java/com/example/papalote_app/screens/Events.kt
@@ -1,10 +1,17 @@
 package com.example.papalote_app.screens
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.papalote_app.components.EventCard
 import com.example.papalote_app.model.getEvents
@@ -15,12 +22,21 @@ fun Events(navController: NavController) {
         navController.popBackStack()
     }
 
-    LazyColumn {
-        items(items = getEvents()) { event ->
-            EventCard(event)
+    Column ()
+    {
+        Text(
+            text = "Eventos",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                color = Color(0xFF1D1B20),
+                fontWeight = FontWeight(600)
+            ),
+            modifier = Modifier.padding(32.dp, 16.dp, 32.dp, 16.dp)
+        )
+        LazyColumn {
+            items(items = getEvents()) { event ->
+                EventCard(event)
+            }
+
         }
-
     }
-
-    Text("Eventos")
 }
